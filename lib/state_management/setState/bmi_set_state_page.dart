eee69f597eda4150eb30e3494c54cf30d9e787bb
Felipe Sales <soufeliposales@gmail.com>
2021-11-21 23:13:33 -0300

    updating and creating the BMIGauge widget

--- lib/state_management/setState/bmi_set_state_page.dart
+++ lib/state_management/setState/bmi_set_state_page.dart
@@ -1,5 +1,6 @@
 import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:state_management/widgets/bmi_gauge.dart';
 
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
@@ -34,55 +35,7 @@ class _BMISetStatePageState extends State<BMISetStatePage> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              SfRadialGauge(
-                axes: [
-                  RadialAxis(
-                    showLabels: true,
-                    showTicks: false,
-                    showAxisLine: false,
-                    minimum: 12.5,
-                    maximum: 47.9,
-                    ranges: [
-                      BMIGaugeRange(
-                        color: Colors.blue,
-                        start: 12.5,
-                        end: 18.5,
-                        label: 'Thinness',
-                      ),
-                      BMIGaugeRange(
-                        color: Colors.green,
-                        start: 18.5,
-                        end: 24.5,
-                        label: 'Normal',
-                      ),
-                      BMIGaugeRange(
-                        color: Colors.yellow[600]!,
-                        start: 24.5,
-                        end: 29.9,
-                        label: 'Overweight',
-                      ),
-                      BMIGaugeRange(
-                        color: Colors.red[500]!,
-                        start: 29.9,
-                        end: 39.9,
-                        label: 'Obesity',
-                      ),
-                      BMIGaugeRange(
-                        color: Colors.red[900]!,
-                        start: 39.9,
-                        end: 47.9,
-                        label: 'Severe Obesity',
-                      ),
-                    ],
-                    pointers: const [
-                      NeedlePointer(
-                        value: 15,
-                        enableAnimation: true,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+              BMIGauge(bmi: 15),
               const SizedBox(height: 20),
               TextFormField(
                 controller: weightController,
--- lib/widgets/template.dart
+++ lib/widgets/template.dart
@@ -5,6 +5,8 @@ import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 import 'package:state_management/widgets/bmi_gauge_range.dart';
 
+import 'bmi_gauge.dart';
+
 class BMISetStatePage extends StatefulWidget {
   const BMISetStatePage({Key? key}) : super(key: key);
 
@@ -34,55 +36,7 @@ class _BMISetStatePageState extends State<BMISetStatePage> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              SfRadialGauge(
-                axes: [
-                  RadialAxis(
-                    showLabels: true,
-                    showTicks: false,
-                    showAxisLine: false,
-                    minimum: 12.5,
-                    maximum: 47.9,
-                    ranges: [
-                      BMIGaugeRange(
-                        color: Colors.blue,
-                        start: 12.5,
-                        end: 18.5,
-                        label: 'Thinness',
-                      ),
-                      BMIGaugeRange(
-                        color: Colors.green,
-                        start: 18.5,
-                        end: 24.5,
-                        label: 'Normal',
-                      ),
-                      BMIGaugeRange(
-                        color: Colors.yellow[600]!,
-                        start: 24.5,
-                        end: 29.9,
-                        label: 'Overweight',
-                      ),
-                      BMIGaugeRange(
-                        color: Colors.red[500]!,
-                        start: 29.9,
-                        end: 39.9,
-                        label: 'Obesity',
-                      ),
-                      BMIGaugeRange(
-                        color: Colors.red[900]!,
-                        start: 39.9,
-                        end: 47.9,
-                        label: 'Severe Obesity',
-                      ),
-                    ],
-                    pointers: const [
-                      NeedlePointer(
-                        value: 15,
-                        enableAnimation: true,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+              const BMIGauge(bmi: 15),
               const SizedBox(height: 20),
               TextFormField(
                 controller: weightController,
