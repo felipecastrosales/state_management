f58aa436b50fb1c9249a92b5b934a17f5ea4e546
Felipe Sales <soufeliposales@gmail.com>
2021-11-23 23:16:18 -0300

    adding BMIValueNotifierPage

---
+++ lib/state_management/value_notifier/bmi_value_notifier_page.dart
@@ -0,0 +1,129 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:intl/intl.dart';
+
+import 'package:state_management/widgets/bmi_gauge.dart';
+
+class BMIValueNotifierPage extends StatefulWidget {
+  const BMIValueNotifierPage({Key? key}) : super(key: key);
+
+  @override
+  _BMIValueNotifiertate createState() => _BMIValueNotifiertate();
+}
+
+class _BMIValueNotifiertate extends State<BMIValueNotifierPage> {
+  final formKey = GlobalKey<FormState>();
+  final weightController = TextEditingController();
+  final heightController = TextEditingController();
+  var bmi = ValueNotifier(0.0);
+
+  Future<void> _calculateBMI({
+    required double weight,
+    required double height,
+  }) async {
+    bmi.value = 0;
+    await Future.delayed(const Duration(milliseconds: 500));
+    bmi.value = weight / pow(height, 2);
+  }
+
+  @override
+  void dispose() {
+    weightController.dispose();
+    heightController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_print
+    print('build');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI | SetState'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ValueListenableBuilder<double>(
+                  valueListenable: bmi,
+                  builder: (_, bmivalue, __) {
+                    // ignore: avoid_print
+                    print('build | ValueListenableBuilder');
+                    return BMIGauge(bmi: bmivalue);
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: weightController,
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Mandatory Weight';
+                    }
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Weight'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true,
+                    ),
+                  ],
+                  onChanged: (String value) {},
+                ),
+                TextFormField(
+                  controller: heightController,
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Mandatory Height';
+                    }
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: 'Height'),
+                  inputFormatters: [
+                    CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true,
+                    ),
+                  ],
+                  onChanged: (String value) {},
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    var formValid = formKey.currentState?.validate() ?? false;
+                    if (formValid) {
+                      var formatter = NumberFormat.simpleCurrency(
+                        locale: 'pt_BR',
+                        decimalDigits: 2,
+                      );
+                      double weight =
+                          formatter.parse(weightController.text) as double;
+                      double height =
+                          formatter.parse(heightController.text) as double;
+                      _calculateBMI(
+                        weight: weight,
+                        height: height,
+                      );
+                    }
+                  },
+                  child: const Text('Calculate BMI'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
